740b466ab6bd1f4381fb3def91002e5f63357850
Selcuk SERT <selcuk.sert@gmail.com>
2023-05-29 23:39:42 +0300

    Added LeafletJS based map to UI

---
+++ ui/components/Map.js
@@ -0,0 +1,33 @@
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'
+import styles from '../styles/Map.module.css';
+import {useEffect, useRef} from 'react';
+
+export default function Map() {
+
+    const locationMap = useRef(null);
+
+    useEffect(() => {
+
+        // To avoid container is already created error on refreshes
+        let container = L.DomUtil.get(locationMap.current);
+        if(container != null){
+            container._leaflet_id = null;
+        }
+
+        let map = L.map(locationMap.current, {
+            draggable: true,
+            dragging: true
+        }).setView([39.9348, 36.8527], 7);
+
+        L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}',{
+            maxZoom: 10,
+            subdomains:['mt0','mt1','mt2','mt3'],
+            attribution: `Map Data &copy; <a href="https://www.google.com/">${new Date().getFullYear()} Google</a>`
+        }).addTo(map);
+    }, []);
+
+    return (
+        <div className={styles.mapContainer} ref={locationMap}/>
+    );
+}
--- ui/pages/index.js
+++ ui/pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../components/Map"), {ssr: false});
 
 export default function Home() {
-    return (
-        <div>
+    return (<div>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <title>Create Next App</title>
@@ -14,8 +16,8 @@ export default function Home() {
 
             <main>
                 <Header title="Map Application"/>
+                <Map/>
                 <Footer content="Copyright &copy; Selcuk SERT | 2023"/>
             </main>
-        </div>
-    )
+        </div>)
 }
